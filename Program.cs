8f97d25ee33976f823b5dda0967359fca1dc8d76
Matheus <matheusborgesribeiro8@gmail.com>
2026-08-12 19:59:01 -0300

    tudao

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
 
-namespace Aula01variaveis
+namespace Aula01variaveis //nome do projeto
 {
     public class Program
     {
