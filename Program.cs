cc22298d3a51024a88ac23c119b97bab3c2d489a
Matheus <matheusborgesribeiro8@gmail.com>
2026-08-12 21:49:36 -0300

    aula03 - modificacao

--- Program.cs
+++ Program.cs
@@ -7,12 +7,59 @@ namespace Aula01variaveis //nome do projeto
     {
         
         static void Main(string[] args)
+
         {
-            //ConcatenarPalavras();
+            
+
+
+        
+            Console.WriteLine("Observe o menu abaixo e digite o número referente a opção desejada: ");
+            Console.WriteLine("1 - Concatenar Palavras");
+            Console.WriteLine("2 - Verificar Dia da semana");
+            Console.WriteLine("3 - Calcular Média");
+            Console.WriteLine("4 - Calcular Tabuada"); 
+
+            int opcaoEscolhida = int.Parse(Console.ReadLine());
+
+            switch (opcaoEscolhida)
+            {
+                case 1:
+                ConcatenarPalavras();
+                break;
+                case 2:
+                VerificarAulaEtec();
+                break;
+                case 3:
+                CalcularMedia();
+                break;
+                case 4:
+                CalcularTabuada();
+                break;
+                default: 
+                Console.WriteLine("Opção Inválida");
+                break;
+
+
+
+            }
+            
+            
+
+
+       
+       
+       
+       
+       
+       
+    }  
+            
+        //ConcatenarPalavras();
             //CalcularMedia();
              //CalcularTabuada();
-             VerificarAulaEtec(); 
-        }
+             //VerificarAulaEtec(); 
+        
+        
         
         public static void VerificarAulaEtec()
         {
@@ -118,10 +165,12 @@ namespace Aula01variaveis //nome do projeto
  
 
         }
-
-
     }
 }
+
+
+
+    
 
 
 
